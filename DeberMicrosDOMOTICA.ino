c95864864f4aa6b45acb02fb2daa77866506bd32
Alexander Guanotoa <49966785+amguanotoac@users.noreply.github.com>
2019-07-07 21:29:37 -0500

    Update DeberMicrosDOMOTICA.ino

--- DeberMicrosDOMOTICA.ino
+++ DeberMicrosDOMOTICA.ino
@@ -2,6 +2,7 @@
 
 byte outPins[]  = {8, 9, 10, 11, 12}; // comedor, sala, dormitorio, banio, cocina. (orden Luces)
 boolean estadoLuz = false; // boolean de encendido y apagado
+String clave;
 
 int sensorLuz = 6;
 
@@ -12,21 +13,30 @@ void setup() {
 }
 
 void loop() {
-  sensorLuz = analogRead(0); //Instrucción para obtener dato analógico
-  if (sensorLuz < 700)  {
-    digitalWrite(sensorLuz, HIGH);
-  } else {
-    digitalWrite(sensorLuz, LOW);
+  if (Serial.available() > 0) {
+    clave = Serial.readString();
+    if (clave == "1234") {
+      Serial.println("contraseña correcta");
+      sensorLuz = analogRead(0); //Instrucción para obtener dato analógico
+      if (sensorLuz < 700)  {
+        digitalWrite(sensorLuz, HIGH);
+      } else {
+        digitalWrite(sensorLuz, LOW);
+      }
+      delay(1000);
+    }
+    else {
+      Serial.println("contraseña incorrecta");
+    }
   }
-  delay(1000);
 }
 
 void encendido() {
   if (sensorLuz == HIGH) {
     for (byte i = 0; i < sizeof(outPins) / sizeof(outPins[0]); i++ ) { //recorremos los pins
-      analogWrite( outPins[i], ((estadoLuz) ? 0 : random(0, 255)) ); //Si está encendido, lo apagamos y viceversa (cuartos aleatorios)
+      analogWrite( outPins[i], ((estadoLuz) ? 0 : random(8, 12)) ); //Si está encendido, lo apagamos y viceversa (cuartos aleatorios)
     }
     estadoLuz = !estadoLuz; //Cambiamos el estado
-    Timer1.setPeriod((estadoLuz) ? 1000000 : 5000000); //Cambiamos el periodo, 1 seg para con el LED encendido, 5 seg con el LED apagado.
+    Timer1.setPeriod((estadoLuz) ? 360000000 : 18000000); //Cambiamos el periodo, 60 min para con la luz encendida, 30 min con la luz apagada.
   }
 }
